97f35c6f360ec6b9cc49655769fb015a99f94e88
Eduardo Henrique <27dhiouz9@mozmail.com>
2024-08-01 19:26:08 -0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if command -v apt &> /dev/null; then pkg_cmd='apt install -y'
-elif command -v dnf &> /dev/null; then pkg_cmd='dnf install -y'
+if command -v apt &> /dev/null; then pkg_cmd='apt install -y python3-nautilus'
+elif command -v dnf &> /dev/null; then pkg_cmd='dnf install -y nautilus-python'
 else echo 'Unsupported, try install manually'; exit 1; fi
 
-sudo $pkg_cmd nautilus-python git
+sudo $pkg_cmd git
 
 mkdir -p ~/.local/share/nautilus-python
 mkdir -p ~/.local/share/nautilus-python/extensions
@@ -15,4 +15,8 @@ git clone https://github.com/eduhds/nautilus_extensions.git \
 cp /tmp/nautilus_extensions/nautilus_*.py \
     ~/.local/share/nautilus-python/extensions
 
-echo 'Install complete'
+if [ "$?" -ne 0 ]; then
+    echo 'Failed install'
+else
+    echo 'Install complete'
+fi
